4f7aca9e253953e303c04d3c0980b3308667bac4
David Portilla <dportillalara@gmail.com>
2022-06-18 00:08:54 -0400

    Pokemon Card created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,51 @@
-import type { NextPage } from 'next'
-import { Layout } from '../components/layouts'
+import { NextPage, GetStaticProps } from "next";
+import { Grid } from "@nextui-org/react";
+import pokeApi from "../api/pokeApi";
+import { Layout } from "../components/layouts";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
 
-
-const Home: NextPage = () => {
-  return (
-    <Layout title='Pokemon List - Pokedex'>
-      <ul>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-      </ul>
-    </Layout>
-  )
+interface Props {
+  pokemons: SmallPokemon[];
 }
 
-export default Home
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  return (
+    <Layout title="Pokemon List - Pokedex">
+      <Grid.Container gap={ 2 } justify='flex-start'>
+        {pokemons.map(({ id, name }) => (
+          <li key={id}>
+            #{id} - {name}
+          </li>
+        ))}
+      </Grid.Container>
+    </Layout>
+  );
+};
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=600");
+
+
+
+  const pokemons: SmallPokemon[] = data.results.map((poke, index) => ({
+    ...poke,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      index + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
+};
+
+export default HomePage;
